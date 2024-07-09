8c5aa8b768b28c3136af4222280d6acb9da0fbd4
PeterYurkovich <peteryurkovich1@gmail.com>
2024-07-09 19:47:02 -0400

    they are watching

--- lua/daily.lua
+++ lua/daily.lua
@@ -3,7 +3,7 @@ local daily = {}
 
 function daily.open_daily()
 	local daily_note_name = os.date("%Y-%m-%d.md")
-	if utils.file_exists("./Business/Red Hat/Notes/Daily Notes/" + daily_note_name) then
+	if utils.file_exists("./Business/Red Hat/Notes/Daily Notes/" .. daily_note_name) then
 		print(daily_note_name)
 	end
 end
