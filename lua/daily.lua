18ba348bcb30672ed9ef27f9f4385a35fbb9fa20
PeterYurkovich <pyurkovi@redhat.com>
2024-07-10 08:07:20 -0400

    Always open daily note

--- lua/daily.lua
+++ lua/daily.lua
@@ -4,10 +4,10 @@ local daily = {}
 function daily.open_daily()
 	local daily_note_name = os.date("%Y-%m-%d.md")
 	local file_path = "Business/Red Hat/Notes/Daily Notes/" .. daily_note_name
-	print(file_path)
 	if utils.file_exists(file_path) then
-		vim.cmd.edit(file_path)
+		print("Daily Note Already Exists")
 	end
+	vim.cmd.edit(file_path)
 end
 
 return daily
